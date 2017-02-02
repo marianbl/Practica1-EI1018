0dcbba981daaef1cc52b2f31f42cfb862e4ee248
marianbl <al341915@uji.es>
2017-02-02 10:29:51 +0100

    Update ejercicio10.sh

--- ejercicio10.sh
+++ ejercicio10.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if test -d /home/$1/tmp # test o [] ???
+if test -d /home/$1/tmp # 
 then
   a=0
-  for fich in /home/$1/tmp; do  
-    if [$fich -eq *.aux]; then
-      if test -f $fich; then # test o [] ???
-	b=´ls -l $fich|sed "s/ */#/g" |cut -f5 d#´
+  cd /home/$1/tmp | ls *aux #Vuelca todos los ficheros *.aux a algun lugar que se necesita saber para hacer el for
+  #for fich in /home/$1/tmp; do  
+    #if [$fich -eq *.aux]; then
+      if test -f $fich; then 
+	b=´ls -l $fich|sed "s/  */#/g" |cut -f5 d#´
 	let a+=b
 	rm $fich
       fi
-    fi
-  done
+    #fi
+  #done
   echo "Se borran ficheros del usuario $1"
   echo "Total de bytes eliminados $b"
 
